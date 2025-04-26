9be90b6fd1b3492150330731157f47b9860349cb
chz <abcde228866@126.com>
2025-04-26 11:48:12 +0800

    代理bean实现和创建bean时可以引用其他bean对象

--- src/test/java/beans/factory/support/DefaultListableBeanFactoryTest.java
+++ src/test/java/beans/factory/support/DefaultListableBeanFactoryTest.java
@@ -1,6 +1,9 @@
 package beans.factory.support;
 
+import beans.PropertyValue;
+import beans.PropertyValues;
 import beans.factory.config.BeanDefinition;
+import beans.factory.config.BeanReference;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -13,6 +16,62 @@ public class DefaultListableBeanFactoryTest extends TestCase {
         beanFactory.registerBeanDefinition("helloService", beanDefinition);
         HelloService helloService = (HelloService) beanFactory.getBean("helloService");
         helloService.sayHello();
+    }
+
+    @Test
+    public void testPopulateBeanWithPropertyValues () {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        PropertyValues propertyValues = new PropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("name","张三"));
+        propertyValues.addPropertyValue(new PropertyValue("age",20));
+        propertyValues.addPropertyValue(new PropertyValue("love","yy"));
+        BeanDefinition beanDefinition=new BeanDefinition(Person.class,propertyValues);
+        beanFactory.registerBeanDefinition("person",beanDefinition);
+        Person person=(Person) beanFactory.getBean("person");
+        person.info();
+
+    }
+    @Test
+    public void testBeanReference () {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        PropertyValues propertyValues = new PropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("color","蓝"));
+        propertyValues.addPropertyValue(new PropertyValue("engine","小米"));
+        BeanDefinition beanDefinitionCar=new BeanDefinition(Car.class,propertyValues);
+
+        propertyValues = new PropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("name","张三"));
+        propertyValues.addPropertyValue(new PropertyValue("age",20));
+        propertyValues.addPropertyValue(new PropertyValue("love","yy"));
+        propertyValues.addPropertyValue(new PropertyValue("car",new BeanReference("car")));
+        BeanDefinition beanDefinitionPerson=new BeanDefinition(Person.class,propertyValues);
+        beanFactory.registerBeanDefinition("person",beanDefinitionPerson);
+        beanFactory.registerBeanDefinition("car",beanDefinitionCar);
+        Person person=(Person) beanFactory.getBean("person");
+        person.info();
+
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+    private String love;
+    private Car car;
+    public void info() {
+        System.out.println(name+" "+age+" "+love+" "+car.getColor()+" "+car.getEngine());
+    }
+}
+
+class Car {
+    private String color;
+    private String engine;
+    public String getColor() {
+        return color;
+    }
+    public String getEngine() {
+        return engine;
     }
 }
 
