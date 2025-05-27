e89cafb61c471efdba96741a242565d45aec3d67
chz <abcde228866@126.com>
2025-05-27 17:08:00 +0800

    实现jdk动态代理

--- src/main/java/aop/framework/adapter/AfterReturningAdviceInterceptor.java
+++ src/main/java/aop/framework/adapter/AfterReturningAdviceInterceptor.java
@@ -1,6 +1,23 @@
 package aop.framework.adapter;
 
+import aop.AfterAdvice;
+import aop.AfterReturningAdvice;
 import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 
 public class AfterReturningAdviceInterceptor implements MethodInterceptor, AfterAdvice {
+
+    private AfterReturningAdvice advice;
+    public AfterReturningAdviceInterceptor() {
+    }
+
+    public AfterReturningAdviceInterceptor(AfterReturningAdvice advice) {
+        this.advice = advice;
+    }
+    @Override
+    public Object invoke(MethodInvocation mi) throws Throwable {
+        Object retVal = mi.proceed();
+        this.advice.afterReturning(retVal, mi.getMethod(), mi.getArguments(), mi.getThis());
+        return retVal;
+    }
 }
--- src/test/java/aop/DynamicProxyTest.java
+++ src/test/java/aop/DynamicProxyTest.java
@@ -1,10 +1,13 @@
 package aop;
 
 import Service.WorldService;
+import Service.WorldServiceAfterReturnAdvice;
 import Service.WorldServiceImpl;
 
 import aop.aspectj.AspectJExpressionPointcutAdvisor;
+import aop.framework.JdkDynamicAopProxy;
 import aop.framework.ProxyFactory;
+import aop.framework.adapter.AfterReturningAdviceInterceptor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,7 +18,7 @@ public class DynamicProxyTest {
         WorldService worldService = new WorldServiceImpl();
         advisedSupport = new ProxyFactory();
         //Advisor是Pointcut和Advice的组合
-        String expression = "execution(* org.springframework.test.service.WorldService.explode(..))";
+        String expression = "execution(* Service.WorldService.explode(..))";
         AspectJExpressionPointcutAdvisor advisor = new AspectJExpressionPointcutAdvisor();
         advisor.setExpression(expression);
         AfterReturningAdviceInterceptor methodInterceptor = new AfterReturningAdviceInterceptor(new WorldServiceAfterReturnAdvice());
@@ -23,5 +26,11 @@ public class DynamicProxyTest {
         TargetSource targetSource = new TargetSource(worldService);
         advisedSupport.setTargetSource(targetSource);
         advisedSupport.addAdvisor(advisor);
+    }
+
+    @Test
+    public void testJdkDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+        proxy.explode();
     }
 }
